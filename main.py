bba2310d408c99f78ea03f9bfc9a40485569c4af
Ruslan Prusakov <ruslan.prusakov@gmail.com>
2023-09-29 21:09:06 +0300

    google write

--- main.py
+++ main.py
@@ -1,76 +1,41 @@
-from __future__ import print_function
+import json
+import gspread
+from gspread import Client, Spreadsheet, Worksheet
 
-import os.path
+from conf import credentials, column_mapping
 
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/12Dpdg_OEQCrWNcMP977-tfhFUX69eHRM7pP_FblMNCk/edit#gid=0'
 
 
+def show_worksheets(sh: Spreadsheet):
+    worksheet = sh.worksheets()
 
 
 
-
-class GoogleSheet:
-    SAMPLE_SPREADSHEET_ID = '12Dpdg_OEQCrWNcMP977-tfhFUX69eHRM7pP_FblMNCk'
-    SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-    SAMPLE_RANGE_NAME = 'Data!A2:AV9'    
-    service = None
+def insert_data(ws: Worksheet):
+    with open('product_data.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    header_rows = ws.row_values(1)
+    data_matrix = []
+    for item in data:
+        row_data = [''] * len(header_rows)
+        for key, value in item.items():
+            if key in header_rows:
+                col_index = header_rows.index(key)
+                row_data[col_index] = value
+        data_matrix.append(row_data)
+    start_row = 2
+    ws.insert_rows(data_matrix, start_row)
     
-    def __init__(self):
-        creds = None
-        if os.path.exists('token.json'):
-            creds = Credentials.from_authorized_user_file('token.json', self.SCOPES)
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file('creds.json', self.SCOPES)
-                creds = flow.run_local_server(port=0)
-            with open('token.json', 'w') as token:
-                token.write(creds.to_json())
-        self.service = build('sheets', 'v4', credentials=creds)
-
-
-    def read_values(self):
-        sheet = self.service.spreadsheets()
-        result = sheet.values().get(spreadsheetId=self.SAMPLE_SPREADSHEET_ID, range=self.SAMPLE_RANGE_NAME).execute()
-        values = result.get('values', [])
-        return values
-    
-    
-    def write_values(self, range, values):
-        data = [{
-            'range': range,
-            'values': values
-        }]
-        body = {
-            'valueInputOption': 'USER_ENTERED',
-            'data': data
-        }
-        result = self.service.spreadsheets().values().batchUpdate(spreadsheetId=self.SAMPLE_SPREADSHEET_ID, body=body).execute()
-        return result
-        
-
-
-
-
 
 
 def main():
-    g = GoogleSheet()
-    test_range = 'A4:B6'
-    test_data = [
-        [1, 2],
-        [4, 5],
-        [8, 9]
-    ]
-    g.write_values(test_range, test_data)
-    print(g.read_values())
+    gc = gspread.service_account_from_dict(credentials)
+    sh = gc.open_by_url(SPREADSHEET_URL)
+    worksheet_list = sh.worksheets()
+    data = sh.worksheet('DATA')
+    insert_data(data)
 
 
-
-
-
+if __name__ == '__main__':
+    main()
--- ozon_async.py
+++ ozon_async.py
@@ -2,9 +2,15 @@ import asyncio
 import aiohttp
 import json
 import logging
+import aiofiles
+from datetime import datetime, timedelta
 from more_itertools import chunked
+import os
 
-from conf import ALL_OZON_HEADERS
+
+
+
+from conf import ALL_OZON_HEADERS, delivery, volume_range
 
 
 
@@ -17,7 +23,10 @@ data_dict = {}
 product_data = []
 
 
-async def get_product_data(session, offer_id, product_id, volume, headers):
+
+
+
+async def get_product_data(session, offer_id, product_id, delivery_coast, headers):
     data = {
         'offer_id': offer_id,
         'product_id': product_id
@@ -26,20 +35,70 @@ async def get_product_data(session, offer_id, product_id, volume, headers):
     url = base_url + method_url
     response = await session.post(url=url, headers=headers, json=data)
     result = await response.json()
+    comission = float(result['result']['commissions'][1]['value'])
+    comission_delivery_coast = round((comission + delivery_coast), 2)
+    price = float(result['result']['price'])
+    marketing_price = float(result['result']['marketing_price'])
     product_data.append(
         {
-            'stock': name,
-            'product_id': product_id,
-            'article': offer_id,
-            'name': result['result']['name'],
-            'stocks': result['result']['stocks']['present'],
-            'commissions': result['result']['commissions'][1]['value'],
-            'price': result['result']['price'],
-            'marketing_price': result['result']['marketing_price'],
-            'volume': volume
+            'Магазин': name,
+            'Ozon Product ID': product_id,
+            'Артикул': offer_id,
+            'Наименование товара': result['result']['name'],
+            'Общее кол-во стоков в ЛК.': result['result']['stocks']['present'],
+            'Комиссия + Логистика': comission_delivery_coast,
+            'Текущая цена продажи': round(price, 2),
+            'Цена с учётом скидки озон': round(marketing_price, 2)
         }
     )
     return product_data
+
+
+
+async def get_analitics(session, client_id, api_key):
+    today = datetime.now() - timedelta(days=1)
+    start_day = today - timedelta(days=30)
+    date_from = start_day.strftime('%Y-%m-%d')
+    date_to = today.strftime('%Y-%m-%d')
+    data = {
+        'date_from': date_from,
+        'date_to': date_to,
+        'dimension': [
+          'day',
+          'sku'  
+        ],
+        'filters': [],
+        'limit': 1000,
+        'metrics': [
+            'revenue',
+            'ordered_units',
+            'hits_view_search',
+            'hits_view_pdp',
+            'hits_view',
+            'session_view_search',
+            'session_view_pdp',
+            'session_view',
+        ]
+    }
+    headers = {
+        'Client-Id': client_id,
+        'Api-Key': api_key
+    }
+    method_url = '/v1/analytics/data'
+    url = base_url + method_url
+    response = await session.post(url=url, headers=headers, json=data)
+    result = await response.json()
+    if not os.path.isfile('analitics.json'):
+        async with aiofiles.open('analitics.json', 'w', encoding='utf-8') as file:
+            await file.write(json.dumps(result['result'], indent=4, ensure_ascii=False))
+    else:
+        async with aiofiles.open('analitics.json', 'a', encoding='utf-8') as file:
+            await file.write(json.dumps(result['result'], indent=2, ensure_ascii=False))
+            await file.write('\n')
+        
+    
+    
+    
 
 
 
@@ -58,7 +117,8 @@ async def get_product_param(session, offer_id, headers):
     volume = None
     for item in result['result']:
         volume = round((item['depth'] / 1000 * item['height'] / 1000 * item['width'] / 1000) * 1000, 3)
-    return volume
+    delivery_coast = volume_range(volume, delivery)
+    return delivery_coast
 
 
 
@@ -76,15 +136,15 @@ async def get_stock(session, client_id, api_key, name):
     url = base_url + method_url
     response = await session.post(url=url, headers=headers, json=data)
     result = await response.json()
-    # print(result['result']['items'])
+
     tasks = []
     for product in result['result']['items']:
-        volume = await get_product_param(session=session, offer_id=product['offer_id'], headers=headers)
-        task = asyncio.create_task(get_product_data(session, offer_id=product['offer_id'], product_id=product['product_id'], headers=headers, volume=volume))
+        delivery_coast = float(await get_product_param(session=session, offer_id=product['offer_id'], headers=headers))
+        task = asyncio.create_task(get_product_data(session, offer_id=product['offer_id'], 
+                                                    product_id=product['product_id'], headers=headers, delivery_coast=delivery_coast))
         tasks.append(task)
     products = await asyncio.gather(*tasks)
         
-       
        
         
 def write_to_file(filename, data):
@@ -101,18 +161,20 @@ def write_to_file(filename, data):
 async def main(client_id, api_key, name):
     async with aiohttp.ClientSession() as session:
         data = await get_stock(session, client_id, api_key, name)
+        analitics = await get_analitics(session, client_id, api_key)
         return data
         
         
         
 
 if __name__ == '__main__':
+
     for name, key in ALL_OZON_HEADERS.items():
         asyncio.run(main(name=name, client_id=key['Client-Id'], api_key=key['Api-Key']))
+    write_to_file('product_data.json', product_data)
     
     print(len(product_data))
-    write_to_file('product_data.json', product_data)
-    with open('product_data.json', encoding='utf-8') as file:
-        data = json.load(file)
-    print(len(data))
+    
+    # for name in shops:
+    #     write_to_file(f'{name}.json', product_data)
     
